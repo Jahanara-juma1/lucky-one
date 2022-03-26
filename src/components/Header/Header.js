131f052ba6520b287545a1858dba32637aab83e3
Jahanara-juma1 <jahanarajuma213@gmail.com>
2022-03-26 07:53:47 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Sunglass for Woman and man</h2>
+        </div>
+    );
+};
+
+export default Header;
