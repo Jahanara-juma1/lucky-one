c0f6f292301c34a90939642069db7a54ef46c409
Jahanara-juma1 <jahanarajuma213@gmail.com>
2022-03-26 13:21:01 +0600

    Fake data addad

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>
+        <div className='header'>
             <h2>Sunglass for Woman and man</h2>
+            <h4>Choose 4 Sunglass</h4>
         </div>
     );
 };
